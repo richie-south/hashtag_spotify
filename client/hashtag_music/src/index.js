5b7a77ea3f14a0535303ac088a57c293d34f72fa
rs222kn <rs222kn@student.lnu.se>
2017-02-09 11:16:42 +0100

    App start with store

---
+++ client/hashtag_music/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import { Provider } from 'react-redux'
+import store from './store'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
